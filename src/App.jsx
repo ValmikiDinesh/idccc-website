d43a321266f8f147f46bd75bd56509858869e814
Dinesh <valmikidineshoffcl@gmail.com>
2026-03-31 23:20:54 +0530

    import change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import ProtectedRoute from "./components/ProtectedRoute";
+
 
 // Importing pages
 import Home from "./pages/Home";
